0012a922a94570b267a55ce93e6d42dd2d9e544c
Vincent Zhu <zhu1230@gmail.com>
2015-09-02 15:46:46 +0800

    remove ActionController::RackDelegation

--- lib/devise/failure_app.rb
+++ lib/devise/failure_app.rb
@@ -6,7 +6,6 @@ module Devise
   # page based on current scope and mapping. If no scope is given, redirect
   # to the default_url.
   class FailureApp < ActionController::Metal
-    include ActionController::RackDelegation
     include ActionController::UrlFor
     include ActionController::Redirecting
 
